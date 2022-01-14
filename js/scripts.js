f86048d8229f83170c5c24d5e097a8c4365e4999
Ryan Bass <theryanbass@gmail.com>
2022-01-14 10:58:13 -0800

    clean up formatting in code

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,43 @@
 $(document).ready(function() {
   $("#mainBody").submit(function(event) {
     event.preventDefault();
+
     $("#missingInfo").hide();
     $("#results").hide();
-    questionOne = parseInt($("#questionOne").val());
-    questionTwo = parseInt($("#questionTwo").val());
-    questionThree = parseInt($("#questionThree").val());
-    questionFour = parseInt($("#questionFour").val());
-    questionFive = parseInt($("#questionFive").val());
+
+    const questionOne = parseInt($("#questionOne").val());
+    const questionTwo = parseInt($("#questionTwo").val());
+    const questionThree = parseInt($("#questionThree").val());
+    const questionFour = parseInt($("#questionFour").val());
+    const questionFive = parseInt($("#questionFive").val());
 
     const poolOurAnswers = questionOne + questionTwo + questionThree + questionFour + questionFive;
     console.log(poolOurAnswers);
 
-      if (questionOne === 0 || questionTwo === 0 || questionThree === 0 || questionFour === 0 || questionFive ===0){
-        console.log("if statement checks out")
+      if (questionOne === 0 || questionTwo === 0 || questionThree === 0 || questionFour === 0 || questionFive === 0){
         $("#missingInfo").toggle();
         return;
       }
+
       if (poolOurAnswers > 0 && poolOurAnswers < 200) {
-        console.log('markup')
         $(".results").text("Markdown");
         $("#results").toggle();
       } 
       if (poolOurAnswers >199 && poolOurAnswers <300) {
         $(".results").text("C#");
+        $("#results").toggle();
       } 
       if (poolOurAnswers >299 && poolOurAnswers <400) {
         $(".results").text("HTML");
+        $("#results").toggle();
       } 
       if (poolOurAnswers >399 && poolOurAnswers <500) {
         $(".results").text("Javascript");
+        $("#results").toggle();
+      } 
+      if (poolOurAnswers >499) {
+        $(".results").text("ERROR! DANGER WILL ROBINSON! DANGER!");
+        $("#results").toggle();
       } 
   });
 });
