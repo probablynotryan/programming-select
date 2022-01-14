d46a67adfc17428863c39cd641c40aa4a285c09a
Ryan Bass <theryanbass@gmail.com>
2022-01-14 10:49:05 -0800

    add branching for result with test

--- js/scripts.js
+++ js/scripts.js
@@ -2,28 +2,44 @@ $(document).ready(function() {
   $("#mainBody").submit(function(event) {
     event.preventDefault();
     $("#missingInfo").hide();
+    $("#results").hide();
     questionOne = parseInt($("#questionOne").val());
     questionTwo = parseInt($("#questionTwo").val());
     questionThree = parseInt($("#questionThree").val());
     questionFour = parseInt($("#questionFour").val());
     questionFive = parseInt($("#questionFive").val());
 
-    console.log(questionFive);  
-    console.log(questionFour);  
-    console.log(questionThree);  
-    console.log(questionTwo);  
-    console.log(questionOne);  
+    const poolOurAnswers = questionOne + questionTwo + questionThree + questionFour + questionFive;
+    console.log(poolOurAnswers);
 
       if (questionOne === 0 || questionTwo === 0 || questionThree === 0 || questionFour === 0 || questionFive ===0){
         console.log("if statement checks out")
         $("#missingInfo").toggle();
+        return;
       }
+      if (poolOurAnswers > 0 && poolOurAnswers < 200) {
+        console.log('markup')
+        $(".results").text("Markdown");
+        $("#results").toggle();
+      } 
+      if (poolOurAnswers >199 && poolOurAnswers <300) {
+        console.log('c#')
+      } 
+      if (poolOurAnswers >299 && poolOurAnswers <400) {
+        console.log('html')
+      } 
+      if (poolOurAnswers >399 && poolOurAnswers <500) {
+        console.log('javascript')
+      } 
   });
 });
 
 function getThisStarted() {
   $("#resume").hide();
   $("#whoops").hide();
+  $("#results").hide();
+  $("#restOfQuestions").hide();
+  $("#missingInfo").hide();
   const letsGetStarted = $("#getStarted").val();
   if (letsGetStarted === "yes") {
     $("#resume").toggle();
@@ -33,14 +49,4 @@ function getThisStarted() {
 };
 function yearResponse() {
 $("#restOfQuestions").show();
-  const currentYear = $("#currentYear").val();
-  if (currentYear === "eighties") {
-    console.log("the 80s");
-  } else if (currentYear === "nineties") {
-    console.log("the 90s");
-  } if (currentYear === "zeroZero") {
-    console.log("the 00s");
-  }   if (currentYear === "thisYear") {
-    console.log("recent year");
-  } 
 };
